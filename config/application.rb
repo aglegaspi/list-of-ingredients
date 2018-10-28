1a90fbe5a7d380364da0c49074fd795af76e212e
Alexander George Legaspi <lex83nyc@gmail.com>
2018-10-28 15:54:35 -0400

    installed devise

--- config/application.rb
+++ config/application.rb
@@ -10,12 +10,14 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+
 
 module ListOfIngredients
   class Application < Rails::Application
@@ -31,5 +33,11 @@ module ListOfIngredients
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Middleware for ActiveAdmin
+    config.middleware.use Rack::MethodOverride
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
